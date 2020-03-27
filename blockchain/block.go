14c8a49ae84e0216b6111f3e95a86790bcc69ed0
Val <val.kaplun@outlook.comn>
2020-03-26 22:33:40 -0400

    Need to fix bug in proof of work hash fuctions

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,10 +1,5 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/sha256"
-)
-
 type BlockChain struct {
 	Blocks []*Block
 }
@@ -13,19 +8,28 @@ type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
+	Nonce    int
 }
 
-// This function creates the hash for the new block
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info) // Place holder sha isnt as good
-	b.Hash = hash[:]            // what does this do
-}
+//NOT NEEDED Hash will be done in proof of work
+// // This function creates the hash for the new block
+// func (b *Block) DeriveHash() {
+// 	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+// 	hash := sha256.Sum256(info) // Place holder sha isnt as good
+// 	b.Hash = hash[:]            // what does this do
+// }
 
 //Creates a block
+
 func CreateBlock(data string, PrevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), PrevHash}
-	block.DeriveHash()
+	block := &Block{[]byte{}, []byte(data), PrevHash, 0}
+	//block.DeriveHash()
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
